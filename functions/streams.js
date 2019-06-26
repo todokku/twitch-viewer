d8ccc7639f42b123d96f6e4403b549424f40adef
Matthew Sinex <matthewsinex@gmail.com>
2019-06-26 10:17:17 -0700

    debug

--- functions/streams.js
+++ functions/streams.js
@@ -6,9 +6,7 @@ const {
 } = process.env;
 
 const getApiUrl = (user_query_string) => {
-  var args = [].splice.call(user_query_string,0);
-  let users = args.join("&user_login=");
-  console.log(users);
+  let users = user_query_string.join("&user_login=");
   let url = `https://api.twitch.tv/helix/streams?user_login=${users}`;
 
   //let url = `https://api.twitch.tv/helix/streams?user_login=billbull`;
@@ -22,17 +20,6 @@ exports.handler = async (event) => {
 
   if (httpMethod === 'GET') {
     const user_login = event.queryStringParameters.user_login;
-
-    return {
-      statusCode: 200,
-      body: user_login,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        "Access-Control-Allow-Headers": "Content-Type",
-        'content-type': 'application/json'
-      }
-    }
-
     const apiUrl = getApiUrl(user_login);
 
     const response = await fetch(
