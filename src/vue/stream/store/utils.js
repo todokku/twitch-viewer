c3fe15dfb0ae2fdeadda7e1050c2df4bb1651f37
Matthew Sinex <matthewsinex@gmail.com>
2020-02-06 13:13:02 -0700

    commit
    adding names to fields

--- src/vue/stream/store/utils.js
+++ src/vue/stream/store/utils.js
@@ -3,7 +3,8 @@ import RunsApi from '../../api/Runs.js'
 export default {
   transformLeaderboard(leaderboard, categories) {
     let category =
-      categories.find(c => c.id == leaderboard.data.category);
+      categories
+      .find(c => c.id == leaderboard.data.category);
 
     leaderboard.data.name = category.name;
     leaderboard.data.runs = RunsApi.mapRunsToView(leaderboard.data.runs, leaderboard.data.players.data);
